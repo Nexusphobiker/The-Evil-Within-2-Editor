09fb273fb843995011d170875fedb4fe34bdd09c
Spodes <spodes1998@gmail.com>
2017-10-19 17:16:27 +0200

    Removed some useless commented code

--- TEW2Editor/Program.cs
+++ TEW2Editor/Program.cs
@@ -20,31 +20,6 @@ namespace TEW2Editor
             MainFormThread.SetApartmentState(ApartmentState.STA);
             MainFormThread.Start();
             Console.WriteLine("MainForm started");
-            //PTR test = new PTR(File.Open("dump", FileMode.Open));
-            /*foreach(var a in test.index)
-            {
-                if (!a.encrypted)
-                {
-                    Console.WriteLine(a.path + " " + a.pkrOffset.ToString("X"));
-                }
-            }*/
-
-
-            //MemStream example
-            /*Stream pkrHandle = File.OpenRead(@"E:\Steam\steamapps\common\TheEvilWithin2\base\st00_main.pkr");
-            PTR.PTRFile ptrFile = test.index[0];
-            byte[] buff = new byte[ptrFile.sizeZipped];
-
-            pkrHandle.Position = ptrFile.pkrOffset;
-            pkrHandle.Read(buff, 0, buff.Length);
-            pkrHandle.Flush();
-            MemoryStream buffStream = new MemoryStream(buff);
-            DeflateStream zStream = new DeflateStream(buffStream, CompressionMode.Decompress, CompressionLevel.Default);
-            byte[] outBuff = new byte[ptrFile.sizeZipped];
-            zStream.Read(outBuff, 0, outBuff.Length);
-            File.WriteAllBytes("secondary", outBuff);*/
-            //Console.WriteLine("Done");
-            //Console.ReadKey();
         }
     }
 }
